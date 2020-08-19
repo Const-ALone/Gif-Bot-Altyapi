5348fbe59095615243aa1776192e578e4cbbc743
AŁonє <67675981+const-Forever@users.noreply.github.com>
2020-08-19 13:11:07 +0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,24 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+module.exports = client => {
+
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} İsmi İle Giriş Yaptım!`);
+  client.user.setStatus("dnd");
+   var oyun = [
+
+        "Darknes Code Gif-Bot Altyapı",
+      
+     
+     
+   ];
+  setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "idle"); 
+        }, 2 * 9000);
+}
